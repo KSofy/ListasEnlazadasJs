d9606dbad8ccda93c0adc614889245f67c4b82ec
Kathya Sofia Melgar Marroquin <kmelgarm2@miumg.edu.gt>
2026-04-17 11:30:08 -0600

    Initial commit: Proyecto base

---
+++ src/structures/simple/SinglyLinkedList.js
@@ -0,0 +1,105 @@
+const SimpleNode = require("./SimpleNode");
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this._size = 0;
+  }
+
+  addFirst(value) {
+    const newNode = new SimpleNode(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    if (this.tail === null) {
+      this.tail = newNode;
+    }
+    this._size++;
+  }
+
+  addLast(value) {
+    const newNode = new SimpleNode(value);
+    if (this.tail === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this._size++;
+  }
+
+  removeFirst() {
+    if (this.head === null) {
+      return null;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    if (this.head === null) {
+      this.tail = null;
+    }
+    this._size--;
+    return value;
+  }
+
+  contains(value) {
+    let current = this.head;
+    while (current !== null) {
+      if (this._isSameValue(current.value, value)) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  countOccurrences(value) {
+    throw new Error(
+      "TODO RETO: Implementar countOccurrences(value) en SinglyLinkedList."
+    );
+  }
+
+  clean() {
+    throw new Error("TODO RETO: Implementar clean() en SinglyLinkedList.");
+  }
+
+  reverseInPlace() {
+    throw new Error(
+      "TODO RETO: Implementar reverseInPlace() en SinglyLinkedList."
+    );
+  }
+
+  removeDuplicates() {
+    throw new Error(
+      "TODO RETO: Implementar removeDuplicates() en SinglyLinkedList."
+    );
+  }
+
+  size() {
+    return this._size;
+  }
+
+  isEmpty() {
+    return this._size === 0;
+  }
+
+  toString() {
+    let out = "[";
+    let current = this.head;
+    while (current !== null) {
+      out += String(current.value);
+      if (current.next !== null) {
+        out += ", ";
+      }
+      current = current.next;
+    }
+    out += "]";
+    return out;
+  }
+
+  _isSameValue(left, right) {
+    return left === right;
+  }
+}
+
+module.exports = SinglyLinkedList;
